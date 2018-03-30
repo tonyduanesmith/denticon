c330f3b7d1a8eebf073a2b43268f47ffd34fea96
Tony Duane Smith <tonyduanesmith@me.com>
2018-03-30 16:50:02 +0100

    Setting gulp up

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
+const babel = require('gulp-babel');
 
 gulp.task('less', () => {
     return gulp.src('src/less/main.less')
         .pipe(less())
         .pipe(gulp.dest('dist/css'))
 });
+
+gulp.task('babelify',()=>{
+    gulp.src('src/js/denticon.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist/js'))
+})
